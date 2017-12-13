991f16695dc81c14838f0d29d118fbdd394504e4
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-12-13 15:36:08 -0800

    playing with different scope levels

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var app = new Vue({
   el: '.app',
   data: {
-    message: 'You loaded this page on ' + new Date().toLocaleString()
+    message: 'You loaded this page on ' + new Date().toLocaleString(),
+    seen: true
   }
 })
 
